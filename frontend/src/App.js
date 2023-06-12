1af734bf78c5c92b12df4a8774bee02dc9da9145
Pratham Shah <Prathamsanjayshah2001@gmail.com>
2023-06-12 23:06:36 +0530

    frontend revert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,5 @@
 import React, { useState, useEffect } from "react";
 const App = () => {
-  const [data, setData] = useState("");
-  const getData = () => {
-    setTimeout(() => {
-      setData("dummy data");
-    }, 5000);
-  };
-
-  const displayData = () => {
-    setTimeout(() => {
-      console.log("data hai: ", data);
-    }, 2000);
-  };
-  useEffect(() => {
-    getData();
-    displayData();
-  }, []);
   return <div>hello</div>;
 };
 
