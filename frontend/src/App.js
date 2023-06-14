027eeb3e3fbcd8dcd798ca66a24c662f7494a874
Pratham Shah <Prathamsanjayshah2001@gmail.com>
2023-06-14 11:21:52 +0530

    frontend typescript

--- frontend/src/App.js
+++
@@ -1,6 +0,0 @@
-import React, { useState, useEffect } from "react";
-const App = () => {
-  return <div>hello</div>;
-};
-
-export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,11 +4,8 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'pop': ['Poppins', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+
